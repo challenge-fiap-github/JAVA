d38df9dec4a1bb5c63e8cfea92bd6239eac6f861
sabrinacafe <sabis476@gmail.com>
2024-10-03 03:03:12 -0300

    Ajustando Classe Procedimento Repository

--- clinica-odontologica/src/main/java/com/odontovision/clinica_odontologica/repository/ProcedimentoRepository.java
+++ clinica-odontologica/src/main/java/com/odontovision/clinica_odontologica/repository/ProcedimentoRepository.java
@@ -15,10 +15,13 @@ import java.util.List;
  * </p>
  */
 @Repository
-public interface ProcedimentoRepository extends JpaRepository<Procedimento, Long>, GenericRepository<Procedimento, Long> {
+public interface ProcedimentoRepository extends JpaRepository<Procedimento, Long> {
 
     /**
      * Lista procedimentos com risco de fraude.
+     * <p>
+     * Busca todos os procedimentos que estão marcados como suspeitos de fraude (riscoFraude = true).
+     * </p>
      *
      * @return Lista de procedimentos com risco de fraude.
      */
@@ -27,6 +30,9 @@ public interface ProcedimentoRepository extends JpaRepository<Procedimento, Long
 
     /**
      * Consulta personalizada para listar procedimentos realizados antes de uma data específica.
+     * <p>
+     * Esta consulta busca todos os procedimentos realizados antes de uma data informada.
+     * </p>
      *
      * @param dataProcedimento Data limite para busca.
      * @return Lista de procedimentos realizados antes da data especificada.
@@ -36,6 +42,9 @@ public interface ProcedimentoRepository extends JpaRepository<Procedimento, Long
 
     /**
      * Consulta personalizada para buscar procedimentos por tipo.
+     * <p>
+     * Retorna todos os procedimentos de um tipo específico, como "Limpeza" ou "Extração".
+     * </p>
      *
      * @param tipoProcedimento Tipo do procedimento.
      * @return Lista de procedimentos de um tipo específico.
@@ -43,7 +52,10 @@ public interface ProcedimentoRepository extends JpaRepository<Procedimento, Long
     List<Procedimento> findByTipoProcedimento(String tipoProcedimento);
 
     /**
-     * Consulta personalizada para buscar procedimentos por custo superior a um valor.
+     * Consulta personalizada para buscar procedimentos com custo superior a um valor.
+     * <p>
+     * Retorna todos os procedimentos cujo custo seja superior ao valor especificado.
+     * </p>
      *
      * @param custo Valor mínimo do custo do procedimento.
      * @return Lista de procedimentos com custo superior ao valor especificado.
